165b421d667cceeaa230958f21d9e890abc41bc1
Kang Soyeong <a44895575@gmail.com>
2024-03-25 12:23:44 +0900

    0325

--- MVCtest/src/main/java/mybean/controller/BookController.java
+++ MVCtest/src/main/java/mybean/controller/BookController.java
@@ -1,6 +1,8 @@
 package mybean.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +10,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import mybean.model.Book;
 
 //중간에서 통제할 서블릿 만들기.
 @WebServlet("/book")
@@ -25,13 +30,33 @@ public class BookController extends HttpServlet {
 		req.setCharacterEncoding("utf-8");
 		String command = req.getParameter("command");
 		String url ="";
+		
+		//페이지가 생성될 때마다 arrayList가 생기면 전혀 쌓이지 않음.
+		//세션이 없는 경우, 즉 처음일경우에만 ArrayList를 생성해서 저장해주면됨.
+		//setattribute로 세션에 저장했으니까, getattribute는 null이 아니므로 ArrayList는 생성되지않고
+		//세션에 저장되어있는 값이 나옴.
+		//서블릿에서는 세션이렇게 사용해야함.
+		
+		HttpSession session = req.getSession();
+		ArrayList<Book> bookList = (ArrayList)session.getAttribute("bookList");
+		
+
+
 		//그래서 각 요청에 따라 처리해주기.
 		if(command.equals("shop")) {
 			url = "bookshop.jsp";
 			
+			
 		}
 		else if(command.equals("cart")) {
-			url = "cart.jsp";
+			url = "bookshop.jsp";
+			
+			if(bookList == null){
+				bookList = new ArrayList<Book>();
+			}
+			
+			bookList.add(getBook(req));
+			session.setAttribute("bookList", bookList);
 		}
 		
 		else if(command.equals("checkout")) {
@@ -41,6 +66,30 @@ public class BookController extends HttpServlet {
 		//그래서 각 요청페이지로 보내주기.
 		RequestDispatcher view = req.getRequestDispatcher(url);
 		view.forward(req, resp);
+		
+		
+
+		}	
+	
+	
+	public Book getBook(HttpServletRequest req) {
+		String book = req.getParameter("book");
+		String q = req.getParameter("quantity");
+		Book dto = new Book();
+		StringTokenizer token = new StringTokenizer(book, "/");
+		String title = token.nextToken().trim();
+		String author = token.nextToken().trim();
+		String price = token.nextToken().trim();
+		
+		int quantity = Integer.parseInt(q);
+		
+		dto.setTitle(title);
+		dto.setAuthor(author);
+		dto.setPrice(price);
+		dto.setQuantity(quantity);
+		
+		return dto;
+		
 	}
 	
 	
--- MVCtest/src/main/webapp/cart.jsp
+++ MVCtest/src/main/webapp/cart.jsp
@@ -8,56 +8,10 @@
 <meta charset="utf-8">
 <title>Insert title here</title>
 </head>
-<jsp:useBean id="dto" class="mybean.model.Book"/>
-<%! 
-//메서드가 실행될 때마다 생성되면 안되기 때문에 인스턴스 변수로 선언부에 한번만.
-ArrayList<Book> bookList;
+<%
+ ArrayList<Book> bookList = (ArrayList<Book>)session.getAttribute("bookList");
 %>
 <body>
-	<%
-		
-		//bookshop에서 넘겨받은 옵션값 받아오기.
-		String book = request.getParameter("book");
-		String q = request.getParameter("quantity");
-
-
-		/*
-		if(book != null){
-			String[] bookInfo = book.split("/");
-			dto.setTitle(bookInfo[0].trim());
-			dto.setAuthor(bookInfo[1].trim());
-			dto.setPrice(bookInfo[2].trim());
-		}
-		*/
-		if(book != null){
-		
-		StringTokenizer token = new StringTokenizer(book, "/");
-		String title = token.nextToken().trim();
-		String author = token.nextToken().trim();
-		String price = token.nextToken().trim();
-		
-		int quantity = Integer.parseInt(q);
-		
-		dto.setTitle(title);
-		dto.setAuthor(author);
-		dto.setPrice(price);
-		dto.setQuantity(quantity);
-		
-		//페이지가 생성될 때마다 arrayList가 생기면 전혀 쌓이지 않음.
-		//세션이 없는 경우, 즉 처음일경우에만 ArrayList를 생성해서 저장해주면됨.
-		//setattribute로 세션에 저장했으니까, getattribute는 null이 아니므로 ArrayList는 생성되지않고
-		//세션에 저장되어있는 값이 나옴.
-		bookList = (ArrayList)session.getAttribute("bookList");
-		if(bookList == null){
-			bookList = new ArrayList<Book>();
-		}
-		
-		bookList.add(dto);
-		session.setAttribute("bookList", bookList);
-		}
-	%>
-
-
 	<b>현재 주문한 도서</b>
 	<table border="1">
 		<tr>
@@ -77,7 +31,7 @@ ArrayList<Book> bookList;
 		</tr>	
 			
 			<%	
-			}
+				}
 			}
 			%>
 		</tr>
